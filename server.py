e09e59f29a8ff4c56c35491246a497edeaab9766
Panik <vantasi2000@gmail.com>
2022-11-23 00:33:39 +0700

    v4

--- server.py
+++ server.py
@@ -1,35 +1,62 @@
-from http.server import BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
 import os
 import state
 
-class Server(BaseHTTPRequestHandler):
+class S(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
     def do_GET(self):
         logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
-        if self.path == '/':
-            self.path = '/index.html'
-
         try:
             split_path = os.path.splitext(self.path)
             request_extension = split_path[1]
             if request_extension != ".py":
-                f = open(self.path[1:]).read()
+                f = open("index.html").read()
                 self.send_response(200)
                 self.end_headers()
                 self.wfile.write(bytes(f, 'utf-8'))
-                print("jsadask")
             else:
                 f = "File not found"
                 self.send_error(404,f)
         except:
             f = "File not found"
             self.send_error(404,f)
-    
-    def do_Post(self):
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
-        if self.path == '/on':
-            if state.CURRENT_STATE == 0 : 
-                state.CURRENT_STATE = 1
-                self.send_response(200, 'Turned ON')
-        if self.path == '/of':
-            if state.CURRENT_STATE == 1 : state.CURRENT_STATE = 0
+
+    def do_POST(self):
+        #content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        #post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        #logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n", str(self.path), str(self.headers), post_data.decode('utf-8'))
+
+        self._set_response()
+        if self.path == "/on":
+            state.CURRENT_STATE = "1"
+            self.wfile.write("ON".encode("utf-8"))
+        if self.path == "/off":
+            state.CURRENT_STATE = "0"
+            self.wfile.write("OFF".encode("utf-8"))
+        if self.path == "/getState":
+            self.wfile.write(state.CURRENT_STATE.encode("utf-8"))
+
+def run(server_class=HTTPServer, handler_class=S, port=8000):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('localhost', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting httpd...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping httpd...\n')
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
